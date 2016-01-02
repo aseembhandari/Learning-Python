eafdcb164a6852d9d2a129a037c0a44aee3fcb0b
aseembhandari <bhandariaseem@gmail.com>
2016-01-02 12:08:49 -0800

    Calendar

---
+++ Calendar&Time/Calendar.py
@@ -0,0 +1,8 @@
+__author__ = 'aseem'
+#This program demonstrates how to display a calendar
+
+import calendar;
+
+cal = calendar.month(2015, 12)
+print("The calendar for December is here")
+print(cal)
