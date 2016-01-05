51c970340c7967a81a9fb22a6c957de443c3437f
aseembhandari <bhandariaseem@gmail.com>
2016-01-04 22:48:14 -0800

    ReadWrite

---
+++ FileHandling/ReadWrite.py
@@ -0,0 +1,8 @@
+__author__ = 'aseem'
+#This program demonstrates how to read and write a file
+fob = open('C:\\Users\\aseem\\OneDrive\\Documents\\Python\\Test\\Test1.txt', 'w')
+fob.write('Fuck this shit')
+fob.close()
+fob = open('C:\\Users\\aseem\\OneDrive\\Documents\\Python\\Test\\Test1.txt', 'r')
+fob.read()
+
