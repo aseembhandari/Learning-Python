a20eb3f4e369840bb4cb0a7fc21edd8c91e2af39
aseembhandari <bhandariaseem@gmail.com>
2016-01-09 22:48:16 -0800

    ObjectOrientedProgramming

---
+++ OOP/Class.py
@@ -0,0 +1,22 @@
+class Employee:
+    'Common base class for all employees'
+    empCount = 0
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount +=1
+
+    def displayCount(self):
+        print("Total Employee %d" % Employee.empCount)
+
+    def displayEmployee(self):
+        print("Name : ", self.name, ", Salary: ", self.salary)
+
+emp=  Employee("Aseem",8000);
+emp.displayEmployee()
+emp1 = Employee("Rishi", 1000)
+emp1.displayEmployee()
+print("Total Employee %d" % Employee.empCount)
+
+
+
