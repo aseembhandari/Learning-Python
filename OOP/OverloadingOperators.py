27ec5df376d6cadb6356a0a04f6890654915433b
aseembhandari <bhandariaseem@gmail.com>
2016-01-11 09:39:09 -0800

    OperatorOverloading

---
+++ OOP/OverloadingOperators.py
@@ -0,0 +1,14 @@
+class Vecotr:
+    def __init__(self,a,b):
+        self.a =a
+        self.b =b
+
+    def __str__(self):
+        return 'Vector (%d,%d)' % (self.a,self.b)
+
+    def __add__(self, other):
+        return Vecotr(self.a + other.a, self.b + other.b)
+
+v1 = Vecotr(2,10)
+v2 = Vecotr (5,-2)
+print (v1 + v2)
