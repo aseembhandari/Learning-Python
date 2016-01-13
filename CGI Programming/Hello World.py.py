de6bf2450491f7629c2e06136dede4a08cd7692b
aseembhandari <bhandariaseem@gmail.com>
2016-01-12 21:12:58 -0800

    CGI

---
+++ CGI Programming/Hello World.py.py
@@ -0,0 +1,11 @@
+import cgi, cgitb
+# This program will run HelloWorld file in the explorer
+print("Content-type:text/html\n\n")
+print('<html>')
+print('<head>')
+print('<title>Hello Word - First CGI Program</title>')
+print('</head>')
+print('<body>')
+print('<h2>Hello World! This is my first CGI program</h2>')
+print('</body>')
+print('</html>')
